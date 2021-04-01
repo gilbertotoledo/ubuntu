ce1af41b035313e9f00328852256ecbbb8b49710
Gilberto Toledo <ogilbertotoledo@gmail.com>
2021-04-01 19:47:11 -0300

    Create lamp-script.sh

---
+++ lamp-script.sh
@@ -0,0 +1,54 @@
+while true; do
+	read -p "Do you want to install Apache + PHP + Mysql? [Y/N]" cnt1
+	case $cnt1 in
+		[Yy]* ) break;;
+		[Nn]* ) exit;;
+		* ) printf "Please answer Y or N\n";;
+	esac
+done
+
+apt-get -y update && apt-get -y upgrade
+apt-get -y install curl vim 
+apt-get -y install apache2 apache2-utils php libapache2-mod-php php-mysql php-mbstring php-zip php-gd php-json php-curl
+apt-get -y install mysql-server
+
+FIND="index\.php "
+REPLACE=""
+sed -i "0,/$FIND/s/$FIND/$REPLACE/m" /etc/apache2/mods-available/dir.conf
+
+FIND="DirectoryIndex"
+REPLACE="DirectoryIndex index\.php"
+sed -i "0,/$FIND/s/$FIND/$REPLACE/m" /etc/apache2/mods-available/dir.conf
+
+systemctl restart apache2
+
+ufw status
+ufw app list
+ufw allow OpenSSH
+ufw allow http
+ufw allow https
+ufw allow in "Apache Full"
+ufw enable
+
+mysql_secure_installation
+
+echo "Please enter root user MySQL password configured on previous steps: "
+read -sp rootpasswd
+
+echo "Please enter username for PhpMyAdmin user root: "
+read -p pmausername
+
+echo "Please enter password for PhpMyAdmin user root: "
+read -sp pmapassword
+
+mysql -uroot -p${rootpasswd} -e "CREATE USER '${pmausername}'@'localhost' IDENTIFIED BY '${pmapassword};"
+mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON *.* TO '${pmausername}'@'localhost' WITH GRANT OPTION;"
+
+apt-get -y install phpmyadmin
+phpenmod mbstring
+
+sed -i '$a Include /etc/phpmyadmin/apache.conf' /etc/apache2/apache2.conf
+
+systemctl restart apache2
+
+echo "LAMP Install successfull!"
